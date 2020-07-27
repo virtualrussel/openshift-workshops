6b48cb10ac870887e9657f211d0ce7024f75496a
Russel Wilkinson <virtualrussel@gmail.com>
2020-07-27 14:18:22 -0500

    Update index.js
    Changing BEERME to true and RAINBOW to false

--- dc-metro-map/routes/index.js
+++ dc-metro-map/routes/index.js
@@ -6,8 +6,8 @@ var rest = require('restler');
 // CONSTANTS AND HELPERS
 //-----------------------------------------------------------------------------
 var API_KEY_PLACEHOLDER = process.env.WMATA_API_KEY || '2cc419c974f94b5486dd79d698f7d3f6';
-var BEERME = process.env.BEERME || false;
-var RAINBOW = process.env.RAINBOW || true;
+var BEERME = process.env.BEERME || true;
+var RAINBOW = process.env.RAINBOW || false;
 console.log("using WMATA API Key - " + API_KEY_PLACEHOLDER);
 if (BEERME == 'true') { console.log("Beer Me! "); }
 if (RAINBOW == 'true') { console.log("Rainbows! "); }
